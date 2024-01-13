29dfa0a81b1253a552886a66dc583d0e3738ffbe
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-01-13 19:50:52 +0600

    restore data

---
+++ src/app/components/test-count/test-count.component.ts
@@ -0,0 +1,103 @@
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import {
+  Validation,
+  Input,
+  initTE,
+  Datepicker,
+  Select,
+  Modal,
+  Collapse,
+} from 'tw-elements';
+import { MainUIService } from '../../features/services/main-ui.service';
+import { environment } from '../../../environments/environments';
+import { AuthService } from '../../features/services/auth.service';
+
+@Component({
+  selector: 'app-test-count',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgClass],
+  templateUrl: './test-count.component.html',
+  styleUrl: './test-count.component.css'
+})
+export class TestCountComponent implements OnInit, OnDestroy {
+  modelTestCount: any;
+  companies$?: Observable<any[]>;
+  companies: any[] = [];
+  testCount: any = [];
+  cCode: any = environment.cCode;
+  testCountSubscription?: Subscription;
+
+  constructor(private mainUIService: MainUIService, private authService: AuthService) {
+    // Initialize modelTestCount properties
+    this.resetTestCountForm();
+  }
+  ngOnInit(): void {
+    initTE(
+      { Validation, Input, Datepicker, Select, Modal, Collapse },
+      { allowReinits: true }
+    );
+
+    // disable future date
+    const FromDate = document.getElementById(
+      'FromDate-disable-future'
+    );
+    new Datepicker(FromDate, {
+      disableFuture: true,
+    });
+    const ToDate = document.getElementById(
+      'ToDate-disable-future'
+    );
+    new Datepicker(ToDate, {
+      disableFuture: true,
+    });
+  }
+
+  //============================= Test Count =============================
+
+  // Add & Update Test Count
+  onTestCountFormSubmit(): void {
+    if (this.modelTestCount.FromDate && this.modelTestCount.ToDate) {
+      const countData = new FormData();
+      countData.append('FromDate', this.modelTestCount.FromDate);
+      countData.append('ToDate', this.modelTestCount.ToDate);
+
+      this.testCountSubscription = this.mainUIService
+        .companyWiseTest(countData)
+        .subscribe({
+          next: (data: any) => {
+            this.testCount = data.filter((c: { companyID: string; }) => c.companyID.startsWith(environment.cCode));
+            this.companies$ = this.authService.getAllCompany();
+            this.companies$.subscribe((com) => {
+              if (com) {
+                com.map(c => this.testCount.map((d:any) => d.companyID == c.companyID) && this.companies.push({id: c.companyID, name: c.name, count: this.testCount.find((d: any)=> d.companyID == c.companyID)?.total}));
+              }
+              console.log(this.companies);
+            });
+          },
+        });
+    }
+  }
+
+  // Reset Test Count Form
+  resetTestCountForm(): void {
+    this.modelTestCount = {
+      FromDate: '',
+      ToDate: '',
+    };
+    this.resetCompanies();
+  }
+
+  // Reset Test Count Form
+  resetCompanies(): void {
+    this.companies = []
+  }
+
+  //============================= Destroy All Subscription =============================
+  ngOnDestroy(): void {
+    this.testCountSubscription?.unsubscribe();
+  }
+
+}
